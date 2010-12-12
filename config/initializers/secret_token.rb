3e56dc6588b004077be5ef5e8ddd6cd3784de0db
Andrew Cox <drew.cox@gmail.com>
2010-12-12 18:56:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HtmlPlayground::Application.config.secret_token = '2a195a876bc1176c5192576350fbb914d78b0eb721d4234e86e27f1c72380e70eef4e4079fe9b6e129d38b1ef7a71a76306bbafa0ceade2c3fc991e918824b7b'
